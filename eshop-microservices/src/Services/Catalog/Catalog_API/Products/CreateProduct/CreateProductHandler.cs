ef79c90cfc238a836afb02fcdf71f20a211aa532
rafaela montalvao <rafaelainocencioo@gmail.com>
2024-10-16 12:09:50 -0300

    starting Product features

---
+++ eshop-microservices/src/Services/Catalog/Catalog_API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Catalog_API.Products.CreateProduct
+{
+    public record class CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price)
+    : IRequest<CreateProductResult>;
+    public record class CreateProductResult(Guid Id);
+
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Busniess Logic to create a product
+            throw new NotImplementedException();
+        }
+    }
+}
